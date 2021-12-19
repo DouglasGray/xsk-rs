1105d1583848dd211dc7986cea08050b73f3b519
Douglas Gray <dbgray01@gmail.com>
2021-12-19 23:10:16 +0000

    delete old benches, replace with bench for cmp::min vs an if-else for usize only

---
+++ bench/benches/min.rs
@@ -0,0 +1,52 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use rand::Rng;
+use std::cmp;
+
+fn min_cmp(maybe_smaller: usize, maybe_larger: usize) -> usize {
+    cmp::min(maybe_smaller, maybe_larger)
+}
+
+fn min_usize(maybe_smaller: usize, maybe_larger: usize) -> usize {
+    if maybe_smaller < maybe_larger {
+        maybe_smaller
+    } else {
+        maybe_larger
+    }
+}
+
+fn bench_min(c: &mut Criterion) {
+    let num = 1_000_000;
+    let mut rng = rand::thread_rng();
+
+    let smaller: Vec<_> = (0..num).into_iter().map(|_| rng.gen::<usize>()).collect();
+    let larger: Vec<_> = smaller.iter().map(|i| i + 1).collect();
+
+    let pairs: Vec<(usize, usize)> = smaller.into_iter().zip(larger.into_iter()).collect();
+
+    let mut group = c.benchmark_group("min calcs");
+
+    group.bench_with_input(BenchmarkId::new("min_cmp", num), &pairs, |b, pairs| {
+        b.iter(|| {
+            let mut x = 0;
+            for (fst, snd) in pairs {
+                x = min_cmp(*fst, *snd);
+            }
+            criterion::black_box(x);
+        });
+    });
+
+    group.bench_with_input(BenchmarkId::new("min_usize", num), &pairs, |b, pairs| {
+        b.iter(|| {
+            let mut x = 0;
+            for (fst, snd) in pairs {
+                x = min_usize(*fst, *snd);
+            }
+            criterion::black_box(x);
+        });
+    });
+
+    group.finish();
+}
+
+criterion_group!(benches, bench_min);
+criterion_main!(benches);
