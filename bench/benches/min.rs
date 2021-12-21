e57ac9e19626a338c76b16cc2646fe530f983f32
Douglas Gray <dbgray01@gmail.com>
2021-12-21 00:28:05 +0000

    improve (hopefully) min calc benches

--- bench/benches/min.rs
+++ bench/benches/min.rs
@@ -1,54 +1,32 @@
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
-use rand::Rng;
 use std::cmp;
 
-fn min_cmp(maybe_smaller: usize, maybe_larger: usize) -> usize {
-    cmp::min(maybe_smaller, maybe_larger)
+fn min_cmp(fst: usize, snd: usize) -> usize {
+    cmp::min(fst, snd)
 }
 
-fn min_usize(maybe_smaller: usize, maybe_larger: usize) -> usize {
-    if maybe_smaller < maybe_larger {
-        maybe_smaller
+fn min_usize(fst: usize, snd: usize) -> usize {
+    if fst < snd {
+        fst
     } else {
-        maybe_larger
+        snd
     }
 }
 
 fn bench_min(c: &mut Criterion) {
-    let num = 1_000_000;
-    let mut rng = rand::thread_rng();
+    let mut group = c.benchmark_group("min");
 
-    // There are a lot of calls to cmp::min where it's likely one of
-    // them is larger than the other, for example a cursor position
-    // and the length of the underlying buffer. Want to check how
-    // much difference a manual impl where the first branch is
-    // usually hit would make.
-    let smaller: Vec<_> = (0..num).into_iter().map(|_| rng.gen::<usize>()).collect();
-    let larger: Vec<_> = smaller.iter().map(|i| i + rng.gen::<usize>()).collect();
+    for vals in [(1, 0), (0, 1), (1, 1)] {
+        let p = format!("({}, {})", vals.0, vals.1);
 
-    let pairs: Vec<(usize, usize)> = smaller.into_iter().zip(larger.into_iter()).collect();
-
-    let mut group = c.benchmark_group("min calcs");
-
-    group.bench_with_input(BenchmarkId::new("min_cmp", num), &pairs, |b, pairs| {
-        b.iter(|| {
-            let mut x = 0;
-            for (fst, snd) in pairs {
-                x = min_cmp(*fst, *snd);
-            }
-            criterion::black_box(x);
+        group.bench_with_input(BenchmarkId::new("cmp", p.clone()), &vals, |b, vals| {
+            b.iter(|| min_cmp(vals.0, vals.1));
         });
-    });
 
-    group.bench_with_input(BenchmarkId::new("min_usize", num), &pairs, |b, pairs| {
-        b.iter(|| {
-            let mut x = 0;
-            for (fst, snd) in pairs {
-                x = min_usize(*fst, *snd);
-            }
-            criterion::black_box(x);
+        group.bench_with_input(BenchmarkId::new("if_else", p), &vals, |b, vals| {
+            b.iter(|| min_usize(vals.0, vals.1));
         });
-    });
+    }
 
     group.finish();
 }
